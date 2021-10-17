f4aead17a1a6569cbb2f55ba56fa2433d6b36a00
D <prod@packer-output-d4410061-e7a7-4872-9d87-3b808ccf8bee>
2021-10-17 18:19:07 +0000

    Fix fixe phones

--- resources/[non esx]/gcphone/config.lua
+++ resources/[non esx]/gcphone/config.lua
@@ -1,6 +1,4 @@
 FixePhone = {
-  -- Cabine proche du poste de police
-  ['008-0001'] = { name = "Cabine Telephonique", coords = { x = 372.25, y = -965.75, z = 28.58 } },
 }
 
 ShowNumberNotification = true -- Show Number or Contact Name when you receive new SMS
